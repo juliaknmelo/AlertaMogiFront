ca9a3fa84e413dd350b8a8c2c9c117b5ea141bd2
juliaknmelo <ju-kar98@hotmail.com>
2024-12-06 18:17:23 -0300

    att

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://blogtech-lozz.onrender.com'
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarUsuario = async(url: string, dados: object, setDados: Function) => {
